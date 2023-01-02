f30898b94dd98bbbc401b04bbee110ada4d8434d
Bruno Silva Santana <ibrunoss@outlook.com>
2023-01-02 10:22:28 -0300

    feat: header component created

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
