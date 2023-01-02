c5bee2ed74af7a3a60c124eb300217e67c32a4ab
Bruno Silva Santana <ibrunoss@outlook.com>
2023-01-02 11:08:07 -0300

    feat: footer component created

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { AppMaterialModule } from './app-material.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
