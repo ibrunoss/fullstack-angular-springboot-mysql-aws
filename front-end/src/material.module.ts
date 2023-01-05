f6fd310270e26c7104a3d62534ed5dff22f5c457
Bruno Silva Santana <ibrunoss@outlook.com>
2023-01-04 21:29:07 -0300

    chore: update material module

--- front-end/src/material.module.ts
+++ front-end/src/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -10,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
   ],
