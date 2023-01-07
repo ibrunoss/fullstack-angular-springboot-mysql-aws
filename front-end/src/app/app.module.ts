68fbad26f277d31697d11aff038a7fd16ffad724
Bruno Silva Santana <ibrunoss@outlook.com>
2023-01-07 20:11:25 -0300

    feat: route

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,12 +9,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material.module';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
