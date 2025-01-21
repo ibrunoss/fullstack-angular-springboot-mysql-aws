b9eb0c5b912c727c279e4ad27dc1c47ae659a78d
Bruno Silva Santana <ibrunossl@outlook.com>
2025-01-21 16:40:11 -0300

    chore: Adjustments to resume project development

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { MaterialModule } from '../material.module';
+import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
